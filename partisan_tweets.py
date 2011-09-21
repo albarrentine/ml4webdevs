ace22ef7cedc61a3d4d249b6b81259fdbf44b6d6
Al <al@jumo.com>
2011-09-21 01:33:22 -0400

    Hooking partisan tweet classifier up to the base Naive Bayes model and adding stemmer, stopwords, etc.

--- partisan_tweets.py
+++ partisan_tweets.py
@@ -4,6 +4,8 @@ import tweepy
 import nltk
 import yaml
 from itertools import chain
+from naive_bayes import NaiveBayesClassifier
+from functools import partial
 
 from nltk.stem.porter import PorterStemmer
 
@@ -19,55 +21,57 @@ queries = {'DEM': '%s -%s' % (hashtags['DEM'], hashtags['GOP']),
 
 api = tweepy.API()
 
-def features(tweet, hashtag):
-    """ Fill in your own method. Here's an example to get you started """
-    return dict((word.lower(), True) for word in tweet.text.split() if word != hashtag)
+stopwords = set([word.rstrip() for word in open('stopwords.txt')]) | set(['.',',',';','"',"'",'?','(',')',':','-','_','`','...'])
+porter = PorterStemmer()
 
-    
-    
-class PartisanTweetClassifier(object):
-    def __init__(self, output_file=None):
-        self.output_file = output_file
-        self.model = None
+# Regex pattern for splitting tokens
+tokenizer = r'''(?x)
+      ([A-Z]\.)+
+    | \w+(-\w+)*
+    | \$?\d+(\.\d+)?%?
+    | \.\.\.
+    | [][.,;"'?():-_`]
+'''
+
+class PartisanTweetClassifier(NaiveBayesClassifier):
+    def __init__(self, *args, **kw):
+        super(PartisanTweetClassifier, self).__init__(*args, **kw)
         self.training_sets = {}
         self.test_sets = {}
+    
+    @classmethod
+    def features(cls, tweet, label=None):
+        """ Fill in your own method. Here's an example to get you started """
+        
+        text = tweet if isinstance(tweet, basestring) else tweet.text
+        
+        words = [porter.stem(word.lower()) for word in nltk.regexp_tokenize(text, tokenizer)
+                 if word.lower() != hashtags.get(label) and word not in stopwords]
+        
+        f = dict((word, True) for word in words)
+        f.update(dict((' '.join(word), True) for word in nltk.bigrams(words)))
+        #f.update(dict((' '.join(word),True) for word in nltk.trigrams(words)))
+        
+        return f
         
     def fetch_twitter(self):
-        # Get 200 tweets for each hashtag to train and the next 200 for testing the model's accuracy  
         for party, hashtag in hashtags.iteritems():
             print 'Doing party %s' % party
             print 'Fetching first page'
             training_tweets = api.search(q=queries[party], rpp=200)
             max_id=training_tweets[0].id
             
-            res = [(features(tweet, hashtag), party) for tweet in training_tweets]
+            res = [(tweet, party) for tweet in training_tweets]
             
             for page in range(2,8):
                 print 'Fetching page %d' % page
-                res.extend([(features(tweet, hashtag), party) for tweet in api.search(q=queries[party], rpp=200, page=page, max_id=max_id)])  
- 
+                res.extend([(tweet, party) for tweet in api.search(q=queries[party], rpp=200, page=page, max_id=max_id)])  
+
+            # Delete the hashtag itself since it is effectively the label in this case
+            for tweet, party in res:
+                tweet.text = tweet.text.replace(hashtags[party], '')
+            
             middle = len(res)/2
             self.training_sets[party] = res[:middle]
             self.test_sets[party] = res[middle+1:]
-        
-    def train_model(self):
-        self.model = nltk.classify.NaiveBayesClassifier.train(list(chain(*self.training_sets.values())))
-        
-    def classify_tweet(self, tweet):
-        return self.model.classify(features(tweet, None))
-        
-    def classify_text(self, text):
-        return self.model.classify(dict((word.lower(), True) for word in text.split()))
-
-    def save(self):
-        if self.output_file is not None:
-            self.output_file.write(yaml.dump(self.model))
-    
-    def accuracy(self):
-        return nltk.classify.accuracy(self.model, list(chain(*self.test_sets.values())))
-    
-    def salient_features(self):
-        self.model.show_most_informative_features(n=50)
-        
-class NaiveBayesClassifier(object):
-    """Implement your own here"""
+            
